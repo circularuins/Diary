187b64b25688a52384a84631d5288aca5037db2f
circularuins <ubuntu11@circularuins.(none)>
2013-06-27 10:16:49 +0900

    一覧ページへのサイドバーの実装、サイドバーのリンクからURLパラメーターを取り出す

--- lib/Diary/PC/C/Entry.pm
+++ lib/Diary/PC/C/Entry.pm
@@ -2,6 +2,9 @@ package Diary::PC::C::Entry;
 use strict;
 use warnings;
 use utf8;
+use Time::Local 'timelocal';
+
+use Data::Dumper;
 
 sub create {
     my ($class, $c) = @_;
@@ -13,14 +16,49 @@ sub create {
             }
         );
     }
-    return $c->redirect('/entry/show');
+    return $c->redirect('/entry/show_all');
 }
 
-sub show {
+sub show_all {
     my ($class, $c) = @_;
     my $page = $c->req->param('page') || 1;
     my ($entries, $pager) = $c->db->search_with_pager('entry' => {}, {order_by => 'ctime DESC', page => $page, rows => 5});
-    $c->render('entry.tt', { entries => $entries, pager => $pager });
+
+    my ($sec,$min,$hour,$mday,$month,$year,$wday,$stime) = localtime(time());
+    $year += 1900;
+    $month += 1;
+    my @years;
+    my @months;
+    for (my $tmp = $year ; $tmp >= 2011 ; $tmp--) {
+        push(@years, $tmp);
+    }
+    for (my $tmp = $month ; $tmp >= 1 ; $tmp--) {
+        push(@months, $tmp);
+    }
+
+    $c->render('entry.tt', { entries => $entries, pager => $pager, years => \@years, months => \@months });
+}
+
+sub show_month {
+    my ($class, $c) = @_;
+    my $id = $c->{request}->{env}->{QUERY_STRING};
+    my ($month, $year) = $id =~ /month=(\d{1,2})&year=(\d{4})/;
+    $year -= 1900;
+    $month -= 1;
+    my $epoch_from = timelocal(0, 0, 0, 1, $month, $year);
+    my $epock_to;
+    if ($month == 11) {
+        $epock_to = timelocal(0, 0, 0, 1, 0, $year + 1) - 1;
+    }
+    else {
+        $epock_to = timelocal(0, 0, 0, 1, $month + 1, $year) - 1;
+    }
+    print Dumper $epoch_from;
+    print Dumper $epock_to;
+    # dbから期間指定でsearch
+    # サイドバーのないentry_month.ttを作る？
+
+    $c->render('login.tt');
 }
 
 1;
